91ffd9e2e06790874d6149033f025497a1fa149a
Can Yoldas <can.yoldas0@outlook.com>
2021-10-23 09:46:36 +0300

    Initial Commit

---
+++ ApiProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApiProject
+//
+//  Created by Can Yoldas on 23.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
